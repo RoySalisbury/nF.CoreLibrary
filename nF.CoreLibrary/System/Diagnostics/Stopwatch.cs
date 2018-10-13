aba0db72512841719ea877a7228b2b0439a8b709
Roy Salisbury <roys@express-is.net>
2018-10-13 14:10:18 -0700

    Added Diagnostics.Stopwatch class (only for ESP32 due to non global native method)

---
+++ nF.CoreLibrary/System/Diagnostics/Stopwatch.cs
@@ -0,0 +1,142 @@
+﻿//
+// Copyright (c) 2017 The nanoFramework project contributors
+// Portions Copyright (c) Microsoft Corporation.  All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+namespace System.Diagnostics
+{
+    /// <summary>
+    /// Provides a set of methods and properties that you can use to accurately measure elapsed time.
+    /// </summary>
+    public class Stopwatch
+    {
+        private const long TicksPerMillisecond = 10000;
+        private const long TicksPerSecond = TicksPerMillisecond * 1000;
+
+        private long _elapsed;
+        private long _startTimeStamp;
+        private bool _isRunning;
+
+        static Stopwatch()
+        {
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="T:System.Diagnostics.Stopwatch" /> class.</summary>
+        public Stopwatch()
+        {
+            Reset();
+        }
+
+        /// <summary>Starts, or resumes, measuring elapsed time for an interval.</summary>
+        public void Start()
+        {
+            // Calling start on a running Stopwatch is a no-op.
+            if (!_isRunning)
+            {
+                _startTimeStamp = GetTimestamp();
+                _isRunning = true;
+            }
+        }
+
+        /// <summary>Initializes a new <see cref="T:System.Diagnostics.Stopwatch" /> instance, sets the elapsed time property to zero, and starts measuring elapsed time.</summary>
+        /// <returns>A <see cref="T:System.Diagnostics.Stopwatch" /> that has just begun measuring elapsed time.</returns>
+        public static Stopwatch StartNew()
+        {
+            Stopwatch s = new Stopwatch();
+            s.Start();
+            return s;
+        }
+
+        /// <summary>Stops measuring elapsed time for an interval.</summary>
+        public void Stop()
+        {
+            // Calling stop on a stopped Stopwatch is a no-op.
+            if (_isRunning)
+            {
+                long endTimeStamp = GetTimestamp();
+                long elapsedThisPeriod = endTimeStamp - _startTimeStamp;
+                _elapsed += elapsedThisPeriod;
+                _isRunning = false;
+
+                if (_elapsed < 0)
+                {
+                    // When measuring small time periods the StopWatch.Elapsed* 
+                    // properties can return negative values.  This is due to 
+                    // bugs in the basic input/output system (BIOS) or the hardware
+                    // abstraction layer (HAL) on machines with variable-speed CPUs
+                    // (e.g. Intel SpeedStep).
+
+                    _elapsed = 0;
+                }
+            }
+        }
+
+        /// <summary>Stops time interval measurement and resets the elapsed time to zero.</summary>
+        public void Reset()
+        {
+            _elapsed = 0;
+            _isRunning = false;
+            _startTimeStamp = 0;
+        }
+
+        /// <summary>Stops time interval measurement, resets the elapsed time to zero, and starts measuring elapsed time.</summary>
+        public void Restart()
+        {
+            _elapsed = 0;
+            _startTimeStamp = GetTimestamp();
+            _isRunning = true;
+        }
+
+        /// <summary>Gets a value indicating whether the <see cref="T:System.Diagnostics.Stopwatch" /> timer is running.</summary>
+        /// <returns>
+        ///     <see langword="true" /> if the <see cref="T:System.Diagnostics.Stopwatch" /> instance is currently running and measuring elapsed time for an interval; otherwise, <see langword="false" />.</returns>
+        public bool IsRunning
+        {
+            get { return _isRunning; }
+        }
+
+        /// <summary>Gets the total elapsed time measured by the current instance.</summary>
+        /// <returns>A read-only <see cref="T:System.TimeSpan" /> representing the total elapsed time measured by the current instance.</returns>
+        public TimeSpan Elapsed
+        {
+            get { return new TimeSpan(GetRawElapsedTicks()); }
+        }
+
+        /// <summary>Gets the total elapsed time measured by the current instance, in milliseconds.</summary>
+        /// <returns>A read-only long integer representing the total number of milliseconds measured by the current instance.</returns>
+        public long ElapsedMilliseconds
+        {
+            get { return GetRawElapsedTicks() / TicksPerMillisecond; }
+        }
+
+        /// <summary>Gets the total elapsed time measured by the current instance, in timer ticks.</summary>
+        /// <returns>A read-only long integer representing the total number of timer ticks measured by the current instance.</returns>
+        public long ElapsedTicks
+        {
+            get { return GetRawElapsedTicks(); }
+        }
+
+        /// <summary>Gets the current number of ticks in the timer mechanism.</summary>
+        /// <returns>A long integer representing the tick counter value of the underlying timer mechanism.</returns>
+        public static long GetTimestamp()
+        {
+            return System.Environment.TickCount;
+        }
+
+        // Get the elapsed ticks.        
+        private long GetRawElapsedTicks()
+        {
+            long timeElapsed = _elapsed;
+
+            if (_isRunning)
+            {
+                // If the StopWatch is running, add elapsed time since
+                // the Stopwatch is started last time. 
+                long currentTimeStamp = GetTimestamp();
+                long elapsedUntilNow = currentTimeStamp - _startTimeStamp;
+                timeElapsed += elapsedUntilNow;
+            }
+            return timeElapsed;
+        }
+    }
+}
---
+++ nF.CoreLibrary/System/Environment.cs
@@ -0,0 +1,54 @@
+﻿//
+// Copyright (c) 2017 The nanoFramework project contributors
+// Portions Copyright (c) Microsoft Corporation.  All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+namespace System
+{
+    /// <summary>Provides information about the current environment and platform.</summary>
+    public static class Environment
+    {
+        /// <summary>Gets the number of processors on the current machine.</summary>
+        /// <returns>The 32-bit signed integer that specifies the number of processors on the current machine. There is no default. If the current machine contains multiple processor groups, this property returns the number of logical processors that are available for use by the common language runtime (CLR).</returns>
+        //public static uint ProcessorCount
+        //{
+        //    get
+        //    {
+        //        try
+        //        {
+        //            return NativeProcessorCount();
+        //        }
+        //        catch
+        //        {
+        //            // Not implemented? We know we have at least 1.
+        //            return 1;
+        //        }
+        //    }
+        //}
+
+        /// <summary>Gets the number of milliseconds elapsed since the system started.</summary>
+        /// <returns>A 64-bit signed integer containing the amount of time in milliseconds that has passed since the last time the computer was started. </returns>
+        public static long TickCount
+        {
+            get
+            {
+                try
+                {
+                    // To be used accross all targets, we would need a common method that could get the number of ticks since the target started.
+                    // The ESP32 gets the number of microseconds since boot. We convert that to ticks for this call.
+                     var microseconds = nanoFramework.Hardware.Esp32.HighResTimer.GetCurrent();
+                    return (long)microseconds * 10;
+                }
+                catch
+                {
+                    // Not implemented? Just return 0.
+                    return 0;
+                }
+            }
+        }
+
+        /// <summary>Gets a <see cref="T:System.Version" /> object that describes the major, minor, build, and revision numbers of the common language runtime.</summary>
+        /// <returns>An object that displays the version of the common language runtime.</returns>
+        //public static Version Version => new Version(1, 0);
+    }
+}
